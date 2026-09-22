a90928e0e1516efa474dba97ef55c76be3009183
mrboiisthebest <williamwindle2009@gmail.com>
2026-09-21 20:05:23 -0700

    (Minor Changes) Sync Repo

--- Main.java
+++ Main.java
@@ -3,3 +3,13 @@ public class Main{
         System.out.println("test");
     }
 }
+
+/*
+Create Check List Class
+- Where All Tasks Are Stored Under A Check Lists
+- Stores All Created Checklists
+
+Create A Task Class
+- Has Name, IsCompleted, Date Created, Date Finished
+Update(name, IsCompleted, Date Created, Date Finished) if empy then use already existing
+*/
---
+++ Task.java
@@ -0,0 +1,11 @@
+/*
+- Has Name, IsCompleted, Date Created, Date Finished
+Update(name, IsCompleted, Date Created, Date Finished) if empy then use already existing
+*/
+
+
+public class Task {
+    public Task(String Name, String DateCreated){
+
+    }
+}
